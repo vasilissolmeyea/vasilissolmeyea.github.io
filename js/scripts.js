0d250f7371b10f48f669b6e03f277fefda0ea430
vasilissolmeyea <64262188+vasilissolmeyea@users.noreply.github.com>
2020-04-27 11:55:11 +0300

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -590,5 +590,5 @@ function displayContent() {
 
 document.addEventListener("DOMContentLoaded", function() {
     loader = document.getElementById('loader');
-    loadNow(10);
+    loadNow(20);
 });
